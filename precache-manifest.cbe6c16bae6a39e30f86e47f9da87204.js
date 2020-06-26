19b4a40703beb02d0c7acff99946cad6ef25c6b6
sourav2831 <sourav2831@gmail.com>
2020-06-26 21:21:33 +0530

    Updates

---
+++ precache-manifest.cbe6c16bae6a39e30f86e47f9da87204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b53b3b73330a8202e4b571a06b77d69",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "e2cc220e3394c124ef61",
+    "url": "/note-app/static/css/main.eedef01f.chunk.css"
+  },
+  {
+    "revision": "4ed2cedd3a41cf84c61f",
+    "url": "/note-app/static/js/2.02530503.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/note-app/static/js/2.02530503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2cc220e3394c124ef61",
+    "url": "/note-app/static/js/main.c1e5aaf5.chunk.js"
+  },
+  {
+    "revision": "4506bfef541c593f2b72",
+    "url": "/note-app/static/js/runtime-main.7545398e.js"
+  }
+]);
